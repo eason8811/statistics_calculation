69e27ad20b761e5e16e423263887a2fbf125e8ee
eason8811 <3220563569@qq.com>
2023-11-10 19:29:42 +0800

    运算结果证明百分比数据源与绝对价格数据源的结果一样，删除百分比数据源的运算代码
    Signed-off-by: eason8811 <3220563569@qq.com>

--- sta_cal_4.py
+++ sta_cal_4.py
@@ -82,7 +82,7 @@ def get_info(symbol = 'BTCUSDT',limit = 1500,endTime = int(time.time()*1000)):
         # 3.写入数据(一次性写入多行)
         dictWriter.writerows(output)
 
-symbols = ['MANAUSDT', 'MINAUSDT']
+symbols = ['ENJUSDT', 'RDNTUSDT']#['MANAUSDT', 'MINAUSDT']
 print(symbols)
 print(f"total calculate times = {len(symbols)*(len(symbols)-1)/2}")
 check_times = 1
@@ -114,7 +114,7 @@ def back_test(data_org,check_times,grid):
         time.sleep(0.3)'''
     y = np.array(data_org[symbols[0]])
     x = np.array(data_org[symbols[1]])
-    y_x = (y-x)
+    y_x = np.array(y-x)
     dfgls_consq = DFGLS(y_x)
     print('='*60)
     print(f'T-value = {dfgls_consq.stat}')
@@ -123,8 +123,18 @@ def back_test(data_org,check_times,grid):
     print('='*60)
     if dfgls_consq.stat > dfgls_consq.critical_values['1%'] or dfgls_consq.pvalue >= 0.01:
         print('警戒！平稳性即将越界！请及时查看！'*6)
-    plt.plot(y_x)
-    plt.show()
+    m = np.mean(y_x[:-1000])
+    s = np.std(y_x[:-1000])
+    # plt.plot(y_x)
+    # plt.plot([m for i in range(len(y_x))])
+    # plt.plot([m + 3*s for i in range(len(y_x))])
+    # plt.plot([m - 3*s for i in range(len(y_x))])
+    # plt.plot([m + 4*s for i in range(len(y_x))])
+    # plt.plot([m - 4*s for i in range(len(y_x))])
+    # plt.plot([m + s for i in range(len(y_x))])
+    # plt.plot([m - s for i in range(len(y_x))])
+    # plt.show()
+
     # for j in tqdm(range(3*60)):
     #     time.sleep(1)
     check_times += 1
@@ -135,6 +145,13 @@ def back_test(data_org,check_times,grid):
     touch_amount = 0
     pair_list = []
     total_pair_amount = 0
+    rev = 0
+    fee = 0
+    init_marg = 100
+    usable_marg = round(100 / (3 + (4 * st - m) / 0.2357 * 50), 4)*3
+    print(f'最大可用保证金（4std)${usable_marg}')
+    print(f'最大可用保证金比率（4std){round(usable_marg/init_marg,4)*100}%')
+    print(f'止损后剩余保证金（4std)${init_marg - usable_marg / 3 * (4 * st - m) / 0.2357 * 50}')
     for n in range(len(y_x)):
         if y_x[n]-consq_amount > grid*st:
             consq_amount += grid*st
@@ -144,6 +161,11 @@ def back_test(data_org,check_times,grid):
             elif consq_amount in pair_list:
                 pair_list.remove(consq_amount)
                 total_pair_amount += 1
+                rev += abs(each_roi*-1/(consq_amount-m-1)) if consq_amount < m else abs(each_roi*-1/(consq_amount-m+1))
+                fee += abs(each_roi*-1/(consq_amount-m-1))*0.0005*4 \
+                    if consq_amount < m else abs(each_roi*-1/(consq_amount-m+1))*0.0005*4
+                # rev += abs(each_roi) if consq_amount < m else abs(each_roi)
+                # fee += 0.0005 * 4
         elif consq_amount-y_x[n] > grid*st:
             consq_amount -= grid*st
             touch_amount += 1
@@ -152,16 +174,25 @@ def back_test(data_org,check_times,grid):
             elif consq_amount in pair_list:
                 pair_list.remove(consq_amount)
                 total_pair_amount += 1
-    return touch_amount,total_pair_amount,each_roi
+                rev += abs(each_roi*-1/(consq_amount-m-1)) if consq_amount < m else abs(each_roi*-1/(consq_amount-m+1))
+                fee += abs(-1 / (consq_amount - m - 1)) * 0.0005 * 4 \
+                    if consq_amount < m else abs(-1 / (consq_amount - m + 1)) * 0.0005 * 4
+                # rev += abs(each_roi) if consq_amount < m else abs(each_roi)
+                # fee += 0.0005 * 4
+    return touch_amount,total_pair_amount,each_roi,rev,fee
 
 result = []
 result_pair = []
 result_each_roi = []
-for i in range(1,2):
-    touch_amount,total_pair_amount,each_roi = back_test(data_org,1,i/10000000)
+result_rev = []
+result_fee = []
+for i in range(4500,6500):
+    touch_amount,total_pair_amount,each_roi,rev,fee = back_test(data_org,1,i/100000)
     result.append(touch_amount)
     result_pair.append(total_pair_amount)
     result_each_roi.append(each_roi)
+    result_rev.append(rev)
+    result_fee.append(fee)
     print('='*35)
     print(f'i = {i}')
     print(f'touch_amount = {touch_amount}')
@@ -171,18 +202,25 @@ for i in range(1,2):
 result = np.array(result)
 result_pair = np.array(result_pair)
 result_each_roi = np.array(result_each_roi)
-plt.plot(np.array([i for i in range(753490,753740)])/10000000,result)
-plt.plot(np.array([i for i in range(753490,753740)])/10000000,result_pair)
-plt.plot(np.array([i for i in range(753490,753740)])/10000000,result_each_roi)
-plt.plot(np.array([i for i in range(753490,753740)])/10000000,result_each_roi*0.0005*4)
-plt.plot(np.array([i for i in range(753490,753740)])/10000000,-result_pair*0.0005*4+result_pair*result_each_roi)
-plt.plot(np.array([i for i in range(753490,753740)])/10000000,-result_pair*(0.0005+0.000347465)*4+result_pair*result_each_roi)
-print((list(-result_pair*0.0005*4+result_pair*result_each_roi).index(max(-result_pair*0.0005*4+result_pair*result_each_roi))+1)/10000000)
+result_rev = np.array(result_rev)
+# plt.plot(np.array([i for i in range(1,500)])/1000,result,label='result')
+# plt.plot(np.array([i for i in range(1,500)])/1000,result_pair,label='result_pair')
+# plt.plot(np.array([i for i in range(1,1000)])/1000,result_each_roi,label='result_each_roi')
+plt.plot(np.array([i for i in range(4500,6500)])/100000,result_rev,label='result_rev')
+# plt.plot(np.array([i for i in range(1,1000)])/1000,result_each_roi*0.0005*4,label='result')
+# plt.plot(np.array([i for i in range(1,1000)])/1000,-result_pair*0.0005*4+result_rev,label='result')
+plt.plot(np.array([i for i in range(4500,6500)])/100000,-result_pair*0.0005*4+result_pair*result_each_roi,label='linear pos with fee')
+plt.plot(np.array([i for i in range(4500,6500)])/100000,result_pair*result_each_roi,label='linear pos without fee')
+plt.plot(np.array([i for i in range(4500,6500)])/100000,result_fee,label='fee')
+plt.plot(np.array([i for i in range(4500,6500)])/100000,result_rev-result_fee,label='result_rev with fee')
+print((list(-result_pair*0.0005*4+result_pair*result_each_roi).index(max(-result_pair*0.0005*4+result_pair*result_each_roi))+1)/1000)
+plt.legend()
 print((list(-result_pair*(0.0005+0.000347465)*4+result_pair*result_each_roi)
-       .index(max(-result_pair*(0.0005+0.000347465)*4+result_pair*result_each_roi))+1)/10000000)
+       .index(max(-result_pair*(0.0005+0.000347465)*4+result_pair*result_each_roi))+1)/1000)
+plt.grid(True)
 plt.show()
 
-#0.07536
+#0.05948
 
 
 
